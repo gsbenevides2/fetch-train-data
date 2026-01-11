adc776698d2d3235b2b42f1b3b5213759dd036f5
Guilherme da Silva Benevides <git@gui.dev.br>
2026-01-11 00:29:47 -0300

    fix: logs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  logging: {
+    incomingRequests: Bun.env.NODE_ENV === "production",
+    fetches: {
+      fullUrl: Bun.env.NODE_ENV === "production",
+      hmrRefreshes: Bun.env.NODE_ENV === "production",
+    },
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*.gui.dev.br"],
+    },
+  },
 };
 
 export default nextConfig;
